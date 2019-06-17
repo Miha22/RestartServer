ea4df0309f04d6f1c1d6051d0657df396e7cc47f
Deniel Alekseev (M22) <daniel.neo.eu@icloud.com>
2019-06-17 05:02:04 +0300

    Initial Commit
    in process..

---
+++ Plugin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Rocket.Core;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using SDG.Framework.Modules;
+using Steamworks;
+using SDG.Framework.Translations;
+using System.Globalization;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace ConfigureLobbyInfo
+{
+    public class Plugin : RocketPlugin<PluginConfiguration>
+    {
+        private static Plugin Instance;
+        protected override void Load()
+        {
+            if (Configuration.Instance.PluginEnabled)
+            {
+                Instance = this;
+                Logger.Log("ConfigureLobbyInfo loaded!", ConsoleColor.Cyan);
+                ConfigureLobby();
+            }
+            else
+            {
+                Logger.Log("Plugin is disabled in Configuration");
+                this.Unload();
+            }
+        }
+
+        public void ConfigureLobby()
+        {
+            string mode;
+            string perspective;
+            if (Configuration.Instance.HidePlugins)
+                SteamGameServer.SetKeyValue("rocketplugins", "");
+            else
+                SteamGameServer.SetKeyValue("rocketplugins", string.Join(",", Rocket.Core.R.Plugins.GetPlugins().Select(plugin => plugin.Name).ToArray()));
+            
+            switch (Configuration.Instance.Mode.ToLower().Trim())   
+            {
+                case ("easy"):
+                    mode = "EZY";
+                    break;
+                case ("hard"):
+                    mode = "HRD";
+                    break;
+                default:
+                    mode = "NRM"; 
+                    break;
+            }
+            switch (Configuration.Instance.Perspective.ToLower().Trim())
+            {
+                case ("first"):
+                    perspective = "1Pp";
+                    break;
+                case ("third"):
+                    perspective = "3Pp";
+                    break;
+                case ("vehicle"):
+                    perspective = "4Pp";
+                    break;
+                default:
+                    perspective = "2Pp";
+                    break;
+            }
+            string tags = String.Concat(new string[]
+            {
+                Configuration.Instance.IsPVP ? "PVP" : "PVE",
+                ",",
+                Configuration.Instance.HasCheats ? "CHy" : "CHn",
+                ",",
+                mode,
+                ",",
+                perspective,
+                ",",
+                Configuration.Instance.HideWorkshop ? "WSn" : "WSy",
+                ",",
+                Configuration.Instance.IsGold ? "GLD" : "F2P",
+                ",",
+                Configuration.Instance.IsBattlEyeSecure ? "BEy" : "BEn",
+                ",<gm>",
+                Configuration.Instance.Game,
+                "</gm>",
+            });
+            SteamGameServer.SetGameTags(tags);
+        }
+    }
+}
---
+++ PluginConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Rocket.API;
+namespace ConfigureLobbyInfo
+{
+    public class PluginConfiguration : IRocketPluginConfiguration
+    {
+        public bool PluginEnabled;
+        public bool HidePlugins;
+        public bool HideWorkshop;
+        //public bool HideConfig;
+        public bool IsPVP;
+        public bool IsBattlEyeSecure;
+        public string Mode;
+        public bool HasCheats;
+        public string Perspective;
+        public bool IsGold;
+        public string Game;
+
+        public void LoadDefaults()
+        {
+            this.PluginEnabled = true;
+            this.HidePlugins = true;
+            this.HideWorkshop = true;
+            //this.HideConfig = true;
+            this.IsPVP = true;
+            this.IsBattlEyeSecure = true;
+            this.Mode = "normal";
+            this.HasCheats = true;
+            this.Perspective = "both";
+            this.IsGold = false;
+            this.Game = "Survival, survival and again survival...";
+        }
+    }
+}
