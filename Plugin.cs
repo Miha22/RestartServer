eb536d176e465365dbc6074cbdae489aa87f6c92
Deniel Alekseev (M22) <daniel.neo.eu@icloud.com>
2019-06-18 01:55:51 +0300

    Fix
    hide workshop works only on server loaded first time, when you reload server with "rocket reload" it will show all your plugins, shit knows why, I tried my best.

--- Plugin.cs
+++ Plugin.cs
@@ -30,10 +30,26 @@ namespace ConfigureLobbyInfo
             else
             {
                 Logger.Log("Plugin is disabled in Configuration");
-                this.Unload();
+                UnloadPlugin();
             }
         }
+        protected override void Unload()
+        {
+            Level.onPostLevelLoaded -= OnPostLevelLoaded;
+        }
+
+        public static int GetWorkshopCount() =>
+            (String.Join(",", Provider.getServerWorkshopFileIDs().Select(x => x.ToString()).ToArray()).Length - 1) / 120 + 1;
+        public static int GetConfigurationCount() =>
+            (String.Join(",", typeof(ModeConfigData).GetFields()
+            .SelectMany(x => x.FieldType.GetFields().Select(y => y.GetValue(x.GetValue(Provider.modeConfigData))))
+            .Select(x => x is bool v ? v ? "T" : "F" : (String.Empty + x)).ToArray()).Length - 1) / 120 + 1;
+
         public void OnPostLevelLoaded(int a) => ConfigureLobby();
+        public void HideRocketPlugins(bool hide)
+        {
+
+        }
         public void ConfigureLobby()
         {
             string mode;
@@ -42,64 +58,24 @@ namespace ConfigureLobbyInfo
             if (Configuration.Instance.HidePlugins)
                 SteamGameServer.SetKeyValue("rocketplugins", "");
             else
-                SteamGameServer.SetKeyValue("rocketplugins", string.Join(",", Rocket.Core.R.Plugins.GetPlugins().Select(plugin => plugin.Name).ToArray()));
+                SteamGameServer.SetKeyValue("rocketplugins", string.Join(",", R.Plugins.GetPlugins().Select(plugin => plugin.Name).ToArray()));
 
             if (Configuration.Instance.HideWorkshop)
             {
-                SteamGameServer.SetKeyValue("Browser_Workshop_Count", "0");
-                SteamGameServer.SetKeyValue("Browser_Workshop_Line_", "0");
+                SteamGameServer.SetKeyValue("Browser_Workshop_Count", "");
+                SteamGameServer.SetKeyValue("Browser_Workshop_Line_", "");
+                SteamGameServer.SetKeyValue("rocketplugins", "");
             }
             else
             {
-                string empty = string.Empty;
-                for (int index = 0; index < SDG.Unturned.Provider.getServerWorkshopFileIDs().Count; ++index)
-                {
-                    if (empty.Length > 0)
-                        empty += (string)(object)',';
-                    empty += (string)(object)SDG.Unturned.Provider.getServerWorkshopFileIDs()[index];
-                }
-                int num5 = (empty.Length - 1) / 120 + 1;
-                int num6 = 0;
-                SteamGameServer.SetKeyValue("Browser_Workshop_Count", num5.ToString());
-                for (int startIndex = 0; startIndex < empty.Length; startIndex += 120)
-                {
-                    int length = 120;
-                    if (startIndex + length > empty.Length)
-                        length = empty.Length - startIndex;
-                    string pValue = empty.Substring(startIndex, length);
-                    SteamGameServer.SetKeyValue("Browser_Workshop_Line_" + (object)num6, pValue);
-                    ++num6;
-                }
+                SteamGameServer.SetKeyValue("Browser_Workshop_Count", GetWorkshopCount().ToString());
             }
+
             if (Configuration.Instance.HideConfig)
                 SteamGameServer.SetKeyValue("Browser_Config_Count", "0");
             else
-            {
-                string str = string.Empty;
-                foreach (FieldInfo field1 in SDG.Unturned.Provider.modeConfigData.GetType().GetFields())
-                {
-                    object obj1 = field1.GetValue((object)SDG.Unturned.Provider.modeConfigData);
-                    foreach (FieldInfo field2 in obj1.GetType().GetFields())
-                    {
-                        object obj2 = field2.GetValue(obj1);
-                        if (str.Length > 0)
-                            str += (string)(object)',';
-                        str = !(obj2 is bool) ? str + obj2 : str + (!(bool)obj2 ? "F" : "T");
-                    }
-                }
-                int num7 = (str.Length - 1) / 120 + 1;
-                int num8 = 0;
-                SteamGameServer.SetKeyValue("Browser_Config_Count", num7.ToString());
-                for (int startIndex = 0; startIndex < str.Length; startIndex += 120)
-                {
-                    int length = 120;
-                    if (startIndex + length > str.Length)
-                        length = str.Length - startIndex;
-                    string pValue = str.Substring(startIndex, length);
-                    SteamGameServer.SetKeyValue("Browser_Config_Line_" + (object)num8, pValue);
-                    ++num8;
-                }
-            }
+                SteamGameServer.SetKeyValue("Browser_Config_Count", GetConfigurationCount().ToString());
+
             switch (Configuration.Instance.Mode.ToLower().Trim())   
             {
                 case ("easy"):
